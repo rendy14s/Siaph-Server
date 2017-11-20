be6e00a8762c1a2225448a386eb1af0b41d9ad49
Ren <rendy1429@gmail.com>
2017-11-20 16:20:58 +0700

    update server one signal

--- common/models/siaph-documents.js
+++ common/models/siaph-documents.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = function (Siaphdocuments) {
+    let request = require('request');
     let app = require('../../server/server');
+   
 
     /**
      * Find Data Documents Filter FromDoc & toDoc
@@ -19,7 +21,7 @@ module.exports = function (Siaphdocuments) {
                 http: "optionsFromRequest"
             }],
             returns: {
-                arg: 'getDataDocument', type: 'array',
+                arg: 'getDataDocument', type: 'array', root: true
             },
             http: {
                 path: '/getDataDocument',
@@ -28,7 +30,7 @@ module.exports = function (Siaphdocuments) {
         });
 
     Siaphdocuments.getDataDocument = function (params, options, cb) {
-        console.log(params, 'Params Find');
+        // console.log(params, 'Params Find');
         if (params !== null && params !== undefined) {
             Siaphdocuments.find({
                 where: {
@@ -50,5 +52,76 @@ module.exports = function (Siaphdocuments) {
         } else {
             console.log('Data Params Kosong');
         }
+    }
+
+
+    /**
+     * Notifikasi Success Create Disposisi Letter
+     */
+    Siaphdocuments.afterRemote('create', function (ctx, modelInstance, next) {
+        console.log('Proses', ctx.result);
+        let Siaphdepthrole = app.models.SiaphDepthrole;
+        let from = ctx.result.fromDoc;
+        let to =  ctx.result.toDoc;
+        console.log(to, 'TO 1');
+        console.log(from, 'From')
+
+        Siaphdepthrole.findById(from, function (err, result) {
+            console.log(result, 'Data Deptrole');
+            
+            let nameFrom = result.nameRole;
+            console.log(to, 'TO 2');
+            let data = {
+                toDoc: to
+            }
+
+            let dataFilter = [];
+            dataFilter.push({
+                'field': 'tag',
+                'key': 'userid',
+                'relation': '=',
+                'value': result.id
+            });
+            let content = nameFrom + ',' + ' Created New Disposisi For You ';
+            sendMessage(dataFilter, content, data);
+            console.log(content, 'Content');
+        });
+
+        next();
+    });
+
+    var sendMessage = function (device, message, data) {
+
+        var restKey = 'ODg1NzFjMGMtYmMxYS00NDVhLWFhMDUtMzA5Y2RjZWQ4NmJh';
+        var appID = 'dba6c690-57d4-43e6-b894-7da32a5d9357';
+        request(
+            {
+                method: 'POST',
+                uri: 'https://onesignal.com/api/v1/notifications',
+                headers: {
+                    'authorization': 'Basic ' + restKey,
+                    'content-type': 'application/json'
+                },
+                json: true,
+                body: {
+                    'app_id': appID,
+                    'filters': device,
+                    'data': data,
+                    'contents': { en: message }
+                }
+            },
+            function (error, response, body) {
+                try {
+                    if (!body.errors) {
+                        console.log(body);
+                    } else {
+                        console.error('Error:', body.errors);
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+
+            }
+        )
     }
 };
